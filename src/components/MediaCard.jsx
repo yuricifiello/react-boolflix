5f079548d43ac139ed1fca3f007f223aaa313181
yuricifiello <yuricifiello@gmail.com>
2025-06-08 18:59:41 +0200

    fix

--- src/components/MediaCard.jsx
+++ src/components/MediaCard.jsx
@@ -1,14 +1,19 @@
 export default function MediaCard({ item }) {
+  const imageUrl = `https://image.tmdb.org/t/p/w342${item.poster}`;
+  const stars = "⭐".repeat(Math.ceil(item.vote / 2)).padEnd(5, "☆");
+
   let flag = "❓";
   if (item.language === "en") flag = "🇬🇧";
   else if (item.language === "it") flag = "🇮🇹";
 
   return (
-    <div>
-      <h3>{item.title}</h3>
-      <p>Titolo originale: {item.original_title}</p>
-      <p>Lingua: {flag}</p>
-      <p>Voto: {item.vote}</p>
+    <div className="media-card" style={{ backgroundImage: `url(${imageUrl})` }}>
+      <div className="media-overlay">
+        <h3>{item.title}</h3>
+        <p>Titolo originale: {item.originalTitle}</p>
+        <p>Lingua: {flag}</p>
+        <p>Voto: {stars}</p>
+      </div>
     </div>
   );
 }
